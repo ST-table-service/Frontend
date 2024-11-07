008472a831cfaccff16b97d2b057954a30711313
pp.navillera <pp.navillera@gmail.com>
2024-11-08 01:46:06 +0900

    Feat: route signup screen

--- app/index.tsx
+++ app/index.tsx
@@ -2,12 +2,14 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Landing from "./Landing";
 import Login from "./Login";
+import Signup from "./Signup";
 
 const Stack = createNativeStackNavigator();
 export default function Index() {
   return (
     <NavigationContainer independent={true}>
       <Stack.Navigator>
+        <Stack.Screen name="Signup" component={Signup} />
         <Stack.Screen name="Landing" component={Landing} />
         <Stack.Screen name="Login" component={Login} />
       </Stack.Navigator>
