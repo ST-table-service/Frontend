23a53138e03202c553c84814661b8c85d6471205
pp.navillera <pp.navillera@gmail.com>
2024-11-09 16:12:18 +0900

    Feat: make Icon Container as a button

--- app/IconContainer.tsx
+++ app/IconContainer.tsx
@@ -1,16 +1,25 @@
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import styled from "styled-components/native";
+import { RootStackParamList } from "./types";
 
 interface IconContainerProps {
   imageUrl: any;
   text: string;
+  navigation: NativeStackNavigationProp<RootStackParamList>;
+  screen: keyof RootStackParamList;
 }
 
-export default function IconContainer({ imageUrl, text }: IconContainerProps) {
+export default function IconContainer({
+  imageUrl,
+  text,
+  navigation,
+  screen,
+}: IconContainerProps) {
   const IconText = styled.Text`
     text-align: center;
     font-size: 14px;
   `;
-  const IconContainer = styled.View`
+  const IconContainer = styled.TouchableOpacity`
     gap: 10px;
   `;
   const Icon = styled.Image`
@@ -19,7 +28,11 @@ export default function IconContainer({ imageUrl, text }: IconContainerProps) {
   `;
 
   return (
-    <IconContainer>
+    <IconContainer
+      onPress={() => {
+        navigation.navigate(screen);
+      }}
+    >
       <Icon source={imageUrl} resizeMode="contain" />
       <IconText>{text}</IconText>
     </IconContainer>
