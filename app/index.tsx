43ad6d68571cb7349f82cc4c000aef19ef149d03
pp.navillera <pp.navillera@gmail.com>
2024-11-09 17:56:21 +0900

    Feat: wrap the components with SafeAreaProvider

--- app/index.tsx
+++ app/index.tsx
@@ -4,28 +4,31 @@ import Landing from "./Landing";
 import Login from "./Login";
 import Signup from "./Signup";
 import MainTabs from "./MainTabs";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const Stack = createNativeStackNavigator();
 export default function Index() {
   return (
-    <NavigationContainer independent={true}>
-      <Stack.Navigator
-        initialRouteName="MainTabs"
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="Landing" component={Landing} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Signup" component={Signup} />
-        <Stack.Screen
-          name="MainTabs"
-          component={MainTabs}
-          options={{
+    <SafeAreaProvider>
+      <NavigationContainer independent={true}>
+        <Stack.Navigator
+          initialRouteName="MainTabs"
+          screenOptions={{
             headerShown: false,
           }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+        >
+          <Stack.Screen name="Landing" component={Landing} />
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Signup" component={Signup} />
+          <Stack.Screen
+            name="MainTabs"
+            component={MainTabs}
+            options={{
+              headerShown: false,
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
