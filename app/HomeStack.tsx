bb9e8f9dcfb4baf59cd27c2c24f1a6a2239985be
pp.navillera <pp.navillera@gmail.com>
2024-11-12 18:43:06 +0900

    Feat: add mypage and cart page to home stack

--- app/HomeStack.tsx
+++ app/HomeStack.tsx
@@ -3,10 +3,12 @@ import Home from "./Home";
 import Popular from "./Popular";
 import Bobby from "./Bobby";
 import Coupon from "./Coupon";
+import MyPage from "./MyPage";
+import Cart from "./Cart";
 const Stack = createNativeStackNavigator();
 export default function HomeStack() {
   return (
-    <Stack.Navigator initialRouteName="Coupon">
+    <Stack.Navigator initialRouteName="Cart">
       <Stack.Screen
         name="Home"
         component={Home}
@@ -31,8 +33,23 @@ export default function HomeStack() {
         }}
       />
       <Stack.Screen
+        name="Cart"
+        component={Cart}
+        options={{
+          headerShown: false,
+          //   header: (props) => <CustomHeader {...props} />, // Popular 화면에서는 CustomHeader 사용
+        }}
+      />
+      <Stack.Screen
         name="Coupon"
         component={Coupon}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="MyPage"
+        component={MyPage}
         options={{
           headerShown: false,
         }}
