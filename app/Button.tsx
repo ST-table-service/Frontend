4f9ea0ebca79ba8f3420e258190e0a1dc613a4ee
pp.navillera <pp.navillera@gmail.com>
2024-11-15 02:35:47 +0900

    Feat: add server communication handling function

--- app/Button.tsx
+++ app/Button.tsx
@@ -7,37 +7,45 @@ type ButtonProps = {
   title: string;
   navigation?: NativeStackNavigationProp<RootStackParamList>;
   screen: keyof RootStackParamList;
+  params?: any;
   buttonColor?: string;
+  onPress?: () => void; // 추가된 onPress prop
 };
 
 interface CustomButtonProps extends TouchableOpacityProps {
-  bgColor?: string; // bgColor 속성을 선택적으로 정의
+  bgColor?: string;
 }
+
 export default function Button({
   title,
   navigation,
   screen,
+  params,
   buttonColor,
+  onPress,
 }: ButtonProps) {
   const Button = styled.TouchableOpacity<CustomButtonProps>`
     align-items: center;
     background-color: ${(props) => props.bgColor || "#ff5643"};
     border-radius: 20px;
-    padding: 22px 80px; /* 수직 패딩을 22px로 설정하고 수평 패딩은 0으로 설정 */
-    /* min-width: 20px; */
-    /* width: 60px; */
+    padding: 22px 80px;
   `;
+
   const ButtonText = styled.Text`
     color: #ffffff;
     font-size: 16px;
   `;
+
+  const handlePress = () => {
+    if (onPress) {
+      onPress();
+    } else if (navigation) {
+      navigation.navigate(screen, params);
+    }
+  };
+
   return (
-    <Button
-      bgColor={buttonColor}
-      onPress={() => {
-        navigation?.navigate(screen);
-      }}
-    >
+    <Button bgColor={buttonColor} onPress={handlePress}>
       <ButtonText>{title}</ButtonText>
     </Button>
   );
